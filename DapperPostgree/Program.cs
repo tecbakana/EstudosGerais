ef4515350aa3ee88398ad7d811d8efc10e0751aa
marcio.campos <marcio.campos@systax.com.br>
2023-09-30 01:28:15 -0300

    utilizando tuplas no metodo

--- DapperPostgree/Program.cs
+++ DapperPostgree/Program.cs
@@ -12,21 +12,20 @@ namespace DapperPostgree
         private static string connString = "User ID=postgres;Password=su74;Host=localhost;Port=5432;Database=cmsxDB;";
         public static void Main(string[] args)
         {
-            CadastrarCliente("Samanta", 22);
-            CadastrarCliente("adelmo",35);
+            CadastrarCliente(("Eldes", 42));
         }
 
-        public static void CadastrarCliente(string clienteNome, int clienteIdade)
+        public static void CadastrarCliente((string clienteNome, int clienteIdade)usuario)
         {
             int result=0;
             using (var connection = new NpgsqlConnection(connString))
             {
-                var sql = $"insert into tbl_cliente(nome, idade) values('{clienteNome}',{clienteIdade})";
-                if(connection.Query($"select nome from tbl_cliente where nome='{clienteNome}'").Count() <= 0)
+                var sql = $"insert into tbl_cliente(nome, idade) values('{usuario.clienteNome}',{usuario.clienteIdade})";
+                if(connection.Query($"select nome from tbl_cliente where nome='{usuario.clienteNome}'").Count() <= 0)
                     result = connection.Execute(sql) ;
             }
 
-            Console.WriteLine(result==0?$"Usuario {clienteNome} já existe!":$"Usuario {clienteNome} cadastrado com sucesso!");
+            Console.WriteLine(result==0?$"Usuario {usuario.clienteNome} já existe!":$"Usuario {usuario.clienteNome} cadastrado com sucesso!");
             
         }
 
