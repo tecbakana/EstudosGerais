65a3f525402143dce3d2f1d91c9a542c16eff60a
marcio.campos <marcio.campos@systax.com.br>
2023-09-30 01:21:49 -0300

    Limpando o código // implementando Dapper e MongoDB

--- DapperPostgree/Program.cs
+++ DapperPostgree/Program.cs
@@ -12,29 +12,29 @@ namespace DapperPostgree
         private static string connString = "User ID=postgres;Password=su74;Host=localhost;Port=5432;Database=cmsxDB;";
         public static void Main(string[] args)
         {
-            ConsultarCliente("marcio", 35);
-            ConsultarCliente("adelmo",35);
+            CadastrarCliente("Samanta", 22);
+            CadastrarCliente("adelmo",35);
         }
 
-        public static void ConsultarCliente(string clienteNome, int clienteIdade)
+        public static void CadastrarCliente(string clienteNome, int clienteIdade)
         {
-            if (!ValidaCliente(clienteNome))
-            { 
-                using (var connection = new NpgsqlConnection(connString))
-                {
-                    var sql = $"insert into tbl_cliente(nome, idade) values('{clienteNome}',{clienteIdade})";
-                    int result = connection.Execute(sql) ;
-                }
+            int result=0;
+            using (var connection = new NpgsqlConnection(connString))
+            {
+                var sql = $"insert into tbl_cliente(nome, idade) values('{clienteNome}',{clienteIdade})";
+                if(connection.Query($"select nome from tbl_cliente where nome='{clienteNome}'").Count() <= 0)
+                    result = connection.Execute(sql) ;
             }
+
+            Console.WriteLine(result==0?$"Usuario {clienteNome} já existe!":$"Usuario {clienteNome} cadastrado com sucesso!");
+            
         }
 
         public static bool ValidaCliente(string clienteNome)
         {
             using(var conn = new NpgsqlConnection(connString))
             {
-                var sql = $"select nome from tbl_cliente where nome='{clienteNome}'";
-
-                return conn.Query(sql).Count() > 0? true : false;
+                return conn.Query($"select nome from tbl_cliente where nome='{clienteNome}'").Count() > 0? true : false;
             }
         }
     }
---
+++ MongoConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace MongoConsole
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string connectionUri = "mongodb+srv://marciovozes:su1974k1@cluster0.rzxvvtp.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp";
+            var settings = MongoClientSettings.FromConnectionString(connectionUri);
+            // Set the ServerApi field of the settings object to Stable API version 1
+            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+            // Create a new client and connect to the server
+            var client = new MongoClient(settings);
+            // Send a ping to confirm a successful connection
+            try
+            {
+                var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+                Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
